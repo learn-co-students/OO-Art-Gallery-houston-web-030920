b259a56cf4789e19340197b3d44bc9a022fb58fa
isaac-3 <isaacgc0596@gmail.com>
2020-03-18 18:05:50 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,72 @@
+require 'pry'
+
 class Artist
 
   attr_reader :name, :years_experience
+  @@all = []
 
   def initialize(name, years_experience)
     @name = name
-    @years_experience = years_experience
+    @years_experience = years_experience.to_f
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select{|p| p.title if p.artist_name == self}
+  end
+
+  def galleries
+    paintings.map{|p| p.gallery_name.name}.uniq
+    # ag = []
+    # Painting.all.each do |p|
+    #   if p.artist_name == self
+    #     ag << p.gallery_name.name
+    #   end
+    # end
+    # ag.uniq
+  end
+
+  def cities
+    paintings.map{|p| p.gallery_name.city}.uniq
+    # ag = []
+    # Painting.all.each do |p|
+    #   if p.artist_name == self
+    #     ag << p.gallery_name.city
+    #   end
+    # end
+    # ag.uniq
+  end
+
+  def self.total_experience
+    self.all.collect{|a| a.years_experience}.sum
+  end
+
+  def prolific
+    x = 0
+    Painting.all.each do |a|
+      if a.artist_name == self
+        x = (a.artist_name.paintings.length)/(a.artist_name.years_experience)
+      end
+    end
+    x
+  end
+
+  def self.most_prolific
+    most_p = self.all[0] 
+    self.all.each do |a|
+      if a.prolific > most_p.prolific
+        most_p = a.name
+      end
+    end
+    most_p
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
   end
 
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,21 @@
 class Painting
-
-  attr_reader :title, :price
+  attr_reader :title, :price, :artist_name, :gallery_name
 
   @@all = []
 
-  def initialize(title, price)
+  def initialize(title, price, artist_name, gallery_name)
     @title = title
     @price = price
+    @artist_name = artist_name
+    @gallery_name = gallery_name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def self.total_price
+    self.all.collect{|p| p.price}.sum
+  end
 end
