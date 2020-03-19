8488a6ed33601d529efeb7b7360a385258c74e3d
angeloxenakis <angeloxenakis@gmail.com>
2020-03-18 23:31:30 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,44 @@
 class Artist
+  @@all = []
 
   attr_reader :name, :years_experience
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
+
+  def paintings
+    Painting.all.select {|painting| painting.artist == self}
+  end
+
+  def galleries
+    self.paintings.map {|painting| painting.gallery}.uniq
+  end
+
+  def cities
+    self.galleries.map {|gallery| gallery.city}.uniq
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
+  end
+
+  def paintings_per_year
+    self.paintings.length.to_f / years_experience.to_f
+  end
+
+  def self.total_experience
+    self.all.map {|artist| artist.years_experience}.sum 
+  end 
+
+  def self.most_prolific
+    self.all.max_by {|artist| artist.paintings_per_year} 
+  end
+
+  def self.all
+    @@all
+  end 
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,5 +1,32 @@
 require_relative '../config/environment.rb'
 
+# Artists
+picasso = Artist.new("Pablo Picasso", 80)
+monet = Artist.new("Claude Monet", 46)
+ross = Artist.new("Bob Ross", 35)
+
+# Galleries
+cool_gallery = Gallery.new("Cool Gallery", "Houston")
+rad_gallery = Gallery.new("Rad Gallery", "Seattle")
+chill_gallery = Gallery.new("Chill Gallery", "Montreal")
+
+# Paintings
+
+the_old_guitarist = Painting.new("The Old Guitarist", 100000000, picasso, cool_gallery)
+the_weeping_woman = Painting.new("The Weeping Woman", 16000000, picasso, cool_gallery)
+self_portrait = Painting.new("Self-Portrait", 67500000, picasso, cool_gallery)
+guernica = Painting.new("Guernica", 200000000, picasso, rad_gallery)
+the_water_lily_pond = Painting.new("The Water Lily Pond", 43700000, monet, cool_gallery)
+poppies = Painting.new("Poppies", 110700000, monet, rad_gallery)
+sunrise = Painting.new("Impression, Sunrise", 110700000, monet, chill_gallery)
+the_old_mill = Painting.new("The Old Mill", 8000, ross, chill_gallery)
+alaskan_waterfall = Painting.new("Alaskan Waterfall", 13000, ross, chill_gallery)
+
+
+
+
+
+
 
 
 
