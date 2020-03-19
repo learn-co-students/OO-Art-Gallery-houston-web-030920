3739b3c11b3d263eff31e0f32f2e0c418c02c0ce
willfbren <willfbren@gmail.com>
2020-03-19 11:11:17 -0500

    Done.

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,23 @@
 class Painting
 
-  attr_reader :title, :price
+	attr_reader :title, :price, :artist, :gallery
 
-  @@all = []
+  	@@all = []
 
-  def initialize(title, price)
-    @title = title
-    @price = price
-  end
+  	def initialize(title, price, artist, gallery)
+    	@title = title
+		@price = price
+		@artist = artist
+		@gallery = gallery
+    	@@all << self
+  	end
+
+  	def self.all
+    	@@all
+	end
+	  
+	def self.total_price
+		self.all.map {|painting| painting.price}.sum
+	end
 
 end
