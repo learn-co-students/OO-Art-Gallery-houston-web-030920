512a4d2f534e8793623e27868b7182fdc0478746
melkilic <melike180400@gmail.com>
2020-03-24 21:25:11 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,60 @@
 class Artist
 
   attr_reader :name, :years_experience
-
+  @@all = []
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+  
+
+   def self.all
+    @@all
+   end
+
+  def paintings
+    arr= []
+     Painting.all.each do |painting|
+      if painting.artist == self
+        arr << painting
+      end
+     end
+     arr
+  end
+
+  def galleries
+    arr= []
+    Painting.all.each do |painting|
+     if painting.artist == self
+       arr << painting.gallery
+     end
+    end
+    arr
+  end
+
+  def cities
+    paintings.map do |painting|
+      painting.gallery.city
+    end
+  end
+
+  def self.total_experience
+    total= 0
+    self.all.each do |artist|
+      total += artist.years_experience
+    end
+    total
+  end
+
+  def self.most_prolific
+    Artist.all.max_by do |artist|
+      artist.paintings.count / artist.years_experience
+    end
+  end
+
+  def create_painting(title,price,gallery)
+     Painting.new(title,price,self,gallery)
   end
 
 end
